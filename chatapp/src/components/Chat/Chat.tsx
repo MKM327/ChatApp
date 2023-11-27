2872187825e9920ab45f63f97c714e6f764636c1
MKM <kaanmesum@gmail.com>
2023-11-27 12:24:34 +0300

    Refactor Chat component to display messages dynamically

--- chatapp/src/components/Chat/Chat.tsx
+++ chatapp/src/components/Chat/Chat.tsx
@@ -1,61 +1,53 @@
 import { BASE_URL, conversationFetcher, fetcher } from "@/lib/exports";
+import { useParams } from "next/navigation";
+import { Interface } from "readline";
 import useSWR from "swr";
+interface IMessage {
+  id: string;
+  sender: string;
+  receiver: string;
+  messageContent: string;
+  createdAt: string;
+  isRead: boolean;
+}
+type MessageType = "sent" | "coming";
+interface MessageProps {
+  type: MessageType;
+  message: IMessage;
+}
+function Message({ type, message }: MessageProps) {
+  const messageType = type === "sent" ? "self-end" : "self-start";
+  const messageColor = type === "sent" ? "sent-message-color" : "primary-color";
+  const textColor = type === "sent" ? "text-primary-color" : "text-text-color";
+  console.log(messageType, messageColor, textColor);
+  return (
+    <div className={messageType}>
+      <div
+        id="sent-message"
+        className={`bg-${messageColor} rounded-3xl p-3 ${textColor} rounded-tr-none w-fit max-w-[250px]`}
+      >
+        {message.messageContent}
+      </div>
+    </div>
+  );
+}
 export default function Chat() {
-  const { data } = useSWR(
+  const { userId } = useParams();
+  const { data } = useSWR<IMessage[], any>(
     `${BASE_URL}message/Conversation?page=1&limit=10&userId=1&receiverId=2`,
     conversationFetcher
   );
-  console.log("data", data);
   return (
     <div className="p-3 bg-secondary-color flex flex-col gap-5 overflow-y-scroll flex-1">
-      <div className="self-start">
-        <div
-          id="coming-message"
-          className="bg-primary-color rounded-3xl p-3 text-text-color rounded-tl-none w-fit max-w-[250px]"
-        >
-          Lorem lipsum wow works really interesting
-        </div>
-      </div>
-      <div className="self-end">
-        <div
-          id="sent-message"
-          className="bg-sent-message-color rounded-3xl p-3 text-primary-color rounded-tr-none w-fit max-w-[250px]"
-        >
-          Lorem lipsum wow works really interesting
-        </div>
-      </div>
-      <div className="self-start">
-        <div
-          id="coming-message"
-          className="bg-primary-color rounded-3xl p-3 text-text-color rounded-tl-none w-fit max-w-[250px]"
-        >
-          Lorem lipsum wow works really interesting
-        </div>
-      </div>
-      <div className="self-end">
-        <div
-          id="sent-message"
-          className="bg-sent-message-color rounded-3xl p-3 font-semibold text-primary-color rounded-tr-none w-fit max-w-[250px]"
-        >
-          Lorem lipsum wow works really interesting
-        </div>
-      </div>
-      <div className="self-end">
-        <div
-          id="sent-message"
-          className="bg-sent-message-color rounded-3xl font-semibold p-3 text-primary-color rounded-tr-none w-fit max-w-[250px]"
-        >
-          Lorem lipsum wow works really interesting
-        </div>
-      </div>
-      <div className="self-end">
-        <div
-          id="sent-message"
-          className="bg-sent-message-color rounded-3xl font-semibold p-3 text-primary-color rounded-tr-none w-fit max-w-[250px]"
-        >
-          Lorem lipsum wow works really interesting
-        </div>
-      </div>
+      {data?.map((message) => {
+        return (
+          <Message
+            type={message.sender == userId ? "sent" : "coming"}
+            message={message}
+            key={message.id}
+          />
+        );
+      })}
     </div>
   );
 }
